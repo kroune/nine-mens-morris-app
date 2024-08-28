f8fa9390cc4e92f5a07156ae313a2db253769ee1
Vadim <maslovvadim398@gmail.com>
2024-08-28 10:51:24 +0300

    upd for scrollable OGS test

--- app/src/main/kotlin/com/kroune/nineMensMorrisApp/ui/impl/game/OnlineGameScreen.kt
+++ app/src/main/kotlin/com/kroune/nineMensMorrisApp/ui/impl/game/OnlineGameScreen.kt
@@ -10,13 +10,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -59,8 +62,15 @@ fun RenderOnlineGameScreen(
             Navigation.GameEnd(pos)
         )
     }
+
+    val scrollState = rememberScrollState()
+
     AppTheme {
-        Column() {
+        Column(
+            modifier = Modifier
+                .verticalScroll(scrollState)
+                .fillMaxSize()
+        ) {
 
             PlayersUI()
 
@@ -95,19 +105,7 @@ fun RenderOnlineGameScreen(
                 }
             )
             Column {
-                when (isGreen) {
-                    true -> {
-                        Text("You are green")
-                    }
-
-                    false -> {
-                        Text("You are blue")
-                    }
-
-                    null -> {
-                        Text("Waiting for server info")
-                    }
-                }
+                if (isGreen == null) {Text("Waiting for server info")}
             }
         }
 
